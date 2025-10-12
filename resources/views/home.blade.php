46b93b32447c1f243b7e5e6a18f4e10f92df1ed8
unknown <kirakorze300@gmail.com>
2025-10-12 21:42:28 +0300

    Navbar izveide

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+<div class="text-center">
+    <h1>Welcome to FilmFinder</h1>
+    <p>Your personal movie discovery app.</p>
+</div>
+@endsection
